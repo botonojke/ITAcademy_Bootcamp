7ce0ade0cc18f0a4ad10454bed524915062b941b
Vladislav Polivko <82750662+botonojke@users.noreply.github.com>
2023-03-23 18:06:20 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import uvicorn
+from fastapi import FastAPI
+
+from core.config import WEB_PORT, WEB_HOST
+from db.base import database
+
+
+app = FastAPI(title="MY_APP")
+
+
+
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", port=int(WEB_PORT), host=WEB_HOST, reload=True)
